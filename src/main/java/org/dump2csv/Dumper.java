1e4506b144dd4593ca06a7d4ad5019aa9821a003
Andrey Stepantsov <andrey.stepantsov@gmail.com>
2018-07-09 17:52:42 -0700

    check unknown options

--- src/main/java/org/dump2csv/Dumper.java
+++ src/main/java/org/dump2csv/Dumper.java
@@ -20,26 +20,45 @@ public class Dumper {
         System.exit(cli(args));
     }
 
+    public static int options(String[] args) throws IOException {
+        int status = 0;
+        switch (args.length) {
+            case 0:
+                break;
+            case 1:
+                switch (args[0]) {
+                    case "--version":
+                        System.out.println("j-dump2csv, version:" + version());
+                    case "-h":
+                    case "--help":
+                        System.out.println(help());
+                        status = 0;
+                        break;
+                    default:
+                        System.out.println("use -h or --help for information; an unknown option: " + args[0]);
+                        status = 1;
+                }
+                break;
+            default:
+                System.out.println("too many options; use -h or --help for information");
+        }
+        return status;
+    }
+
     public static int cli(String[] args) {
         Logger logger = LoggerFactory.getLogger(Dumper.class);
         int status = 0;
         try {
-            if(1 == args.length) {
-                if ("--version".equals(args[0])) {
-                    System.out.println("j-dump2csv, version:" + version());
-                }
-                if ("--help".equals(args[0]) || "-h".equals(args[0])) {
-                    System.out.println("j-dump2csv, version:" + version());
-                    System.out.println(help());
-                }
-            }else {
+            if (0 != options(args)) {
+                throw new RuntimeException("unknown option"); //TODO: report unknown option
+            } else {
                 Config config = Config.load(System.in);
                 DS ds = openDS(config);
                 ResultSet rs = ds.query(config.query);
                 dump(rs, config.output_format == Config.OUTPUT_FORMAT.CSV);
                 rs.close();
             }
-        }catch (Throwable t){
+        } catch (Throwable t) {
             status = 1;
             logger.error(t.getMessage());
         }
@@ -47,14 +66,13 @@ public class Dumper {
     }
 
     public static void dump(ResultSet rs, boolean useDefault) throws SQLException, IOException {
-        final CSVPrinter printer = (useDefault? CSVFormat.DEFAULT : CSVFormat.TDF)
+        final CSVPrinter printer = (useDefault ? CSVFormat.DEFAULT : CSVFormat.TDF)
                 .withHeader(rs).print(new PrintStream(System.out));
         ResultSetMetaData md = rs.getMetaData();
         int columnCount = md.getColumnCount();
-        while (rs.next())
-        {
+        while (rs.next()) {
             ArrayList<Object> l = new ArrayList(columnCount);
-            for(int i = 1; i <= columnCount; i++){
+            for (int i = 1; i <= columnCount; i++) {
                 l.add(rs.getObject(i));
             }
             printer.printRecord(l);
@@ -63,18 +81,23 @@ public class Dumper {
 
     /**
      * Factory method to open DS
+     *
      * @param config
      * @return
      * @throws SQLException
      */
     static DS openDS(Config config) throws SQLException {
         DS ds = null;
-        switch (config.dbtype){
-            case ORACLE: ds = new OracleDS(); break;
-            case H2: ds = new H2DS(); break;
+        switch (config.dbtype) {
+            case ORACLE:
+                ds = new OracleDS();
+                break;
+            case H2:
+                ds = new H2DS();
+                break;
             case UNKNOWN:
             default:
-                throw new RuntimeException("unsupported DB type: '"+ config.dbtype+"'");
+                throw new RuntimeException("unsupported DB type: '" + config.dbtype + "'");
         }
         ds.open(config);
         return ds;
